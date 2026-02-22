d8d94f2f97c668033ae0616d9cf353371d37ab00
kanishkaanarang <kanishkaanarang@gmail.com>
2026-02-22 19:03:14 +0530

    5th and 6th question

---
+++ two pointer/containerWithMaxWater.py
@@ -0,0 +1,18 @@
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+        left = 0
+        right = len(height) - 1
+        maxWater = 0
+
+        while left<right:
+            width = right - left
+            h = min(height[left], height[right])
+            currWater = width * h
+
+            maxWater = max(maxWater, currWater)
+            if height[left] < height[right]:
+                left += 1
+            else:
+                right -= 1
+        
+        return maxWater
